875124d0111775d4b650940a1842299f2b4d23f2
MC Villoso <mcvilloso@gmail.com>
2022-11-19 20:33:10 -0800

    Updated Code

---
+++ Develop/config/connection.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const Sequelize = require('sequelize');
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(
+    'ecommerce_db', 
+    'root', 
+    'C0dingisfun!',
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
+
+module.exports = sequelize;
